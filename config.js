caae3d641e182d659e5c965cf13fa0a7d1c81bc9
Stackbit Code Editor <projects@stackbit.com>
2021-05-09 18:00:48 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Orion Project',
-  subHeading: 'Rethink the Movie expirience ',
+  subHeading: 'Rethink the Movie experience ',
 
   // social
   socialLinks: [

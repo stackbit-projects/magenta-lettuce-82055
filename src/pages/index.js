d18de609cacaf114ca7115fc92f60e750ef06a85
giorgosn <sdi1400127@di.uoa.gr>
2021-05-09 18:05:32 +0000

    index.js: updated Header in div

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ const IndexPage = () => (
       <div className="container">
         <div className="row">
           <div className="col-lg-8 mx-auto">
-            <h2 className="text-white mb-4">Built with Bootstrap 4</h2>
+            <h2 className="text-white mb-4">Take cinema experience everywhere</h2>
             <p className="text-white-50">
               Grayscale is a free Bootstrap theme created by Start Bootstrap. It
               can be yours right now, simply download the starter on

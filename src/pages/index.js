3502d7a642c374e6bf8e7ae6fac20317fdb0d7fa
giorgosn <sdi1400127@di.uoa.gr>
2021-05-09 18:06:44 +0000

    index.js: updated Paragraph in Paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ const IndexPage = () => (
           <div className="col-lg-8 mx-auto">
             <h2 className="text-white mb-4">Take cinema experience everywhere</h2>
             <p className="text-white-50">
-              We provide
+              We provide our 
               <a href="https://github.com/anubhavsrivastava/gatsby-starter-grayscale">
                 the github repo
               </a>
